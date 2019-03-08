21d1e1ed26bdf1b7557c46005b1b26d71c6255a3
bolzo45 <your-nbolzoni@eracom.educanet2.ch>
2019-03-08 11:21:28 +0100

    code gauche droite (raté)

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//ce code sert à bouger le +
 $(".ecran1").append("<div class='plus' id='plus1'>+</div>");
 //code animation trouver ici: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_animate_3
 function animation() {
@@ -14,3 +15,27 @@ function animation() {
     }
   }
 }
+
+
+//ce code sert à faire bouger le personage droite/gauche
+//flèche droite
+$('body').on('keypress',function ( event ) {
+
+				var myChar = String.fromCharCode(event.which);
+				if ( event.keyCode == '39' ) {
+					//colorChange();
+					console.log("réussi");
+				}
+				else {
+				console.log("raté");
+					// On pourrait produire un son d'erreur.
+					function checkKey(e) {
+
+
+		}
+
+
+}
+				}
+
+			});
