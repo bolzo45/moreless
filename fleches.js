4307930af3bb92b2aa4a236105387f50c4f68cba
Luca Keller <lkeller@eracom.educanet2.ch>
2019-04-05 11:45:43 +0200

    deplacement

--- fleches.js
+++ fleches.js
@@ -6,23 +6,44 @@
 $('body').on('keydown',function ( event ) {
 
     var toucheClavier = event.which;
+	//détécter la largeur du navigateur
+var largeurEcran = document.documentElement.clientWidth;
+var stopDroite = largeurEcran - ( largeurEcran / 11.1111 );
+
+	// Détecter position du personnage:
+	persoPosition = $('.personnage').offset();
+	$("#debug").html("left: " + persoPosition.left + "<br>ecran: "+largeurEcran+"px" + "<br>limite: "+ stopDroite);
+
 
     if ( toucheClavier == 39 ) {
+
+		if ( persoPosition.left < largeurEcran - ( largeurEcran / 11.1111 )) {
+			console.log("flèche gauche");
+	        $(".personnage").animate(
+				{left: "-=11.1111%"},
+				100
+			);
 
         console.log("flèche droite");
         // superFlash();
 
         // Déplacement du personnage
-        $(".personnage").animate({
-            left: "+=11.1111%"
-        });
+        $(".personnage").animate(
+			{left: "+=11.1111%"},
+			100
+		);
 
     } else if ( toucheClavier == 37 ) {
 
-        console.log("flèche gauche");
-        $(".personnage").animate({
-            left: "-=11.1111%"
-        });
+		if ( persoPosition.left > 10) {
+			console.log("flèche gauche");
+	        $(".personnage").animate(
+				{left: "-=11.1111%"},
+				100
+			);
+
+		}
+
     }
 
     else {
@@ -40,4 +61,3 @@ function superFlash() {
    });
 
 }
-
