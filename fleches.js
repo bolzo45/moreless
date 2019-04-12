bc5a2157243dde3787618825ac97f3776a64bcc3
Manuel Schmalstieg <git.code@ms-studio.net>
2019-04-12 11:15:08 +0200

    On réapplique l'état précédent

--- fleches.js
+++ fleches.js
@@ -3,54 +3,60 @@
 // Utiliser l'événement "keydown", car "keypress" ne tient pas compte des flèches
 // selon https://stackoverflow.com/questions/19347269/
 
+largeurEcran = document.documentElement.clientWidth;
+stopDroite = largeurEcran - ( largeurEcran / 5 );
+
+
+function morelessDebug() {
+
+  // Détecter position du personnage:
+  persoPosition = $('.personnage').offset();
+  $("#debug").html("left: " + persoPosition.left + "<br>ecran: "+largeurEcran+"px" + "<br>limite: "+ stopDroite);
+
+}
+
 $('body').on('keydown',function ( event ) {
 
     var toucheClavier = event.which;
-	//détécter la largeur du navigateur
-	var largeurEcran = document.documentElement.clientWidth;
-	var stopDroite = largeurEcran - ( largeurEcran / 11.1111 );
+  //détécter la largeur du navigateur
 
-	// Détecter position du personnage:
-	persoPosition = $('.personnage').offset();
-	$("#debug").html("left: " + persoPosition.left + "<br>ecran: "+largeurEcran+"px" + "<br>limite: "+ stopDroite);
 
-	// Note:
-	// flèche gauche 37
-	// flèche droite: 39
+  morelessDebug();
+
+  // Note:
+  // flèche gauche 37
+  // flèche droite: 39
     if ( toucheClavier == 37 ) {
 
-		// largeurEcran - ( largeurEcran / 11.1111 )
+    // largeurEcran - ( largeurEcran / 11.1111 )
 
-		morelessDebug();
+    morelessDebug();
 
-		if ( persoPosition.left > 10 ) {
-	        $(".personnage").animate(
-				{left: "-=11.1111%"},
-				100
-			);
+    if ( persoPosition.left > 10 ) {
+          $(".personnage").animate(
+        {left: "-=11.1111%"},
+        100
+      );
 
-		}
-		//
-        // console.log("flèche droite");
-        // // superFlash();
-		//
-        // Déplacement du personnage
-        // $(".personnage").animate(
-		// 	{left: "+=11.1111%"},
-		// 	100
-		// );
+      morelessDebug();
 
     }
 
-	else if ( toucheClavier == 39 ) {
+    }
 
-		if ( persoPosition.left < stopDroite) {
-	        $(".personnage").animate(
-				{left: "+=11.1111%"},
-				100
-			);
+  else if ( toucheClavier == 39 ) {
 
-		}
+    morelessDebug();
+
+    if ( persoPosition.left < stopDroite) {
+          $(".personnage").animate(
+        {left: "+=11.1111%"},
+        100
+      );
+
+      morelessDebug();
+
+    }
 
     }
 
