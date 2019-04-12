00ec9bff545987584969f11acdbe528603176631
Luca Keller <lkeller@eracom.educanet2.ch>
2019-04-12 09:16:28 +0200

    déplacement

--- fleches.js
+++ fleches.js
@@ -7,38 +7,42 @@ $('body').on('keydown',function ( event ) {
 
     var toucheClavier = event.which;
 	//détécter la largeur du navigateur
-var largeurEcran = document.documentElement.clientWidth;
-var stopDroite = largeurEcran - ( largeurEcran / 11.1111 );
+	var largeurEcran = document.documentElement.clientWidth;
+	var stopDroite = largeurEcran - ( largeurEcran / 11.1111 );
 
 	// Détecter position du personnage:
 	persoPosition = $('.personnage').offset();
 	$("#debug").html("left: " + persoPosition.left + "<br>ecran: "+largeurEcran+"px" + "<br>limite: "+ stopDroite);
 
-
-    if ( toucheClavier == 39 ) {
+	// Note:
+	// flèche gauche 37
+	// flèche droite: 39
+    if ( toucheClavier == 37 ) {
 
 		if ( persoPosition.left < largeurEcran - ( largeurEcran / 11.1111 )) {
-			console.log("flèche gauche");
 	        $(".personnage").animate(
 				{left: "-=11.1111%"},
 				100
 			);
 
-        console.log("flèche droite");
-        // superFlash();
-
+		}
+		//
+        // console.log("flèche droite");
+        // // superFlash();
+		//
         // Déplacement du personnage
-        $(".personnage").animate(
-			{left: "+=11.1111%"},
-			100
-		);
+        // $(".personnage").animate(
+		// 	{left: "+=11.1111%"},
+		// 	100
+		// );
 
-    } else if ( toucheClavier == 37 ) {
+    }
 
-		if ( persoPosition.left > 10) {
-			console.log("flèche gauche");
+	else if ( toucheClavier == 39 ) {
+
+		if ( persoPosition.left < stopDroite) {
 	        $(".personnage").animate(
-				{left: "-=11.1111%"},
+				{left: "+=11.1111%"},
 				100
 			);
 
